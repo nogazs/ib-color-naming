f4f06e59907ca61f186f34aecad479e20ea9defa
Noga <33994235+nogazs@users.noreply.github.com>
2020-05-12 01:13:11 -0400

    retrieve model files from url, add m_hat

--- ib_naming_model.py
+++ ib_naming_model.py
@@ -1,10 +1,30 @@
+import os
 import pickle
+from urllib.request import urlretrieve
+from zipfile import ZipFile
 from tools import *
 
+LOGGER = get_logger('ib_naming_model')
+DEFAULT_MODEL_URL = 'https://www.dropbox.com/s/70w953orv27kz1o/IB_color_naming_model.zip?dl=1'
 
-def load_model(filename):
+
+def load_model(filename=None):
+    if not os.path.isdir('./models'):
+        os.mkdir('./models')
+    if filename is None:
+        filename = './models/IB_color_naming_model/model.pkl'
+        if not os.path.isfile(filename):
+            LOGGER.info('downloading default model from %s  ...' % DEFAULT_MODEL_URL)
+            urlretrieve(DEFAULT_MODEL_URL, './models/temp.zip')
+            LOGGER.info('extracting model files ...')
+            with ZipFile('./models/temp.zip', 'r') as zf:
+                zf.extractall('./models')
+                os.remove('./models/temp.zip')
+                os.rename('./models/IB_color_naming_model/IB_color_naming.pkl', filename)
     with open(filename, 'rb') as f:
-        return pickle.load(f)
+        LOGGER.info('loading model from file: %s' % filename)
+        model_data = pickle.load(f)
+        return IBNamingModel(**model_data)
 
 
 class IBNamingModel(object):
@@ -19,18 +39,48 @@ class IBNamingModel(object):
         self.qW_M_orig = None
         self.F = IB_curve[0] - betas * IB_curve[1]
 
+    def m_hat(self, qW_M):
+        """
+        :param qW_M: encoder (naming system)
+        :return: optimal decoder (Bayesian listener) that corresponds to the encoder
+        """
+        pMW = qW_M * self.pM
+        pM_W = pMW.T / pMW.sum(axis=0)[:, None]
+        return pM_W.dot(self.pY_M)
+
     def complexity(self, pW_M):
+        """
+        :param pW_M: encoder (naming system)
+        :return: I(M;W)
+        """
         return MI(pW_M * self.pM)
 
     def accuracy(self, pW_M):
+        """
+        :param pW_M: encoder (naming system)
+        :return: I(W;U)
+        """
         pMW = pW_M * self.pM
         pWU = pMW.T @ self.pU_M
         return MI(pWU)
 
     def d_IB(self, pW_M):
+        """
+        :param pW_M: encoder (naming system)
+        :return: E[D[m||m_hat]] = I(M;U) - I(W;U)
+        """
         return self.I_MU - self.accuracy(pW_M)
 
     def fit(self, pW_M):
+        """
+        fits the naming system by
+        :param pW_M: encoder (naming system)
+        :return:
+            epsilon - deviation from optimality of pW_M
+            gnid - gNID between qW_M and qW_M_fit
+            bl - beta_l, the value of beta that was fitted to pW_M
+            qW_M_fit - the optimal IB system at bl
+        """
         Fl = self.complexity(pW_M) - self.betas * self.accuracy(pW_M)
         dFl = Fl - self.F
         bl_ind = dFl.argmin()
@@ -39,4 +89,3 @@ class IBNamingModel(object):
         qW_M_fit = self.qW_M[bl_ind]
         gnid = gNID(pW_M, qW_M_fit, self.pM)
         return epsilon, gnid, bl, qW_M_fit
-
--- main.py
+++ main.py
@@ -1,14 +1,28 @@
-import pickle
 import numpy as np
-import pandas as pd
 import matplotlib.pyplot as plt
 import ib_naming_model
+from tools import *
+
+LOGGER = get_logger('main')
 
 
 def main():
-    model = ib_naming_model.load_model('ib_color_naming.pkl')
+    # load model
+    model = ib_naming_model.load_model()
     curve = model.IB_curve
+
+    # theoretical bound
     plt.plot(curve[0], curve[1])
+    plt.xlabel('complexity, $I(M;W)$')
+    plt.ylabel('accuracy, $I(W;U)$')
+    plt.xlim([0, H(model.pM)])
+    plt.ylim([0, model.I_MU + 0.1])
+
+    # generate fake data and fir to the mdoel
+    pW_M_fake = np.random.rand(330, 10)
+    pW_M_fake /= pW_M_fake.sum(axis=1)[:, None]
+    print(model.complexity(pW_M_fake), model.accuracy(pW_M_fake))
+    epsilon, gnid, bl, qW_M_fit = model.fit(pW_M_fake)
 
 
 if __name__ == '__main__':
